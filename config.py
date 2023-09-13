2f7c960d762d0da9cfda74d4f9aa14b9590fe873
Rasul <rasul.islyamgali@gmail.com>
2023-09-14 01:20:34 +0600

    update: spam_filter

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
     TG_BOT_API_TOKEN: str
+    DEFAULT_BAN_DAYS: int = 10
 
 
 settings = Settings()
--- tgbot/handlers.py
+++ tgbot/handlers.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ContentType
 
@@ -13,9 +15,20 @@ spam_filter_classes = get_spam_filter_classes()
 
 @dp.message_handler(content_types=ContentType.TEXT)
 async def spam_filter(message: types.Message):
+    """if is spam ban user and delete message"""
+
     for cls in spam_filter_classes:
         is_spam = cls(message.text).filter()
 
         if is_spam:
-            await message.reply("Это спам")
+            user_id = message.from_user.id
+            chat_id = message.chat.id
+
+            await message.bot.ban_chat_member(
+                chat_id=chat_id,
+                user_id=user_id,
+                until_date=dt.datetime.today() + dt.timedelta(settings.DEFAULT_BAN_DAYS)
+            )
+            await message.delete()
+
             break
